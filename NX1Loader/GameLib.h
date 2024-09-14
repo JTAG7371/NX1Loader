0d898f34a1ac926ff245c65daeeb713f024fb3fe
JTAG <jtag@jtag.dev>
2024-09-14 02:41:42 +0100

    Add support for every executable

--- NX1Loader/GameLib.h
+++ NX1Loader/GameLib.h
@@ -32,6 +32,87 @@ public:
 	void* (*CallOriginal)(...);
 };
 
+typedef struct _XEX_EXECUTION_ID { 
+	DWORD MediaID; // 0x0 sz:0x4
+	DWORD Version; // 0x4 sz:0x4
+	DWORD BaseVersion; // 0x8 sz:0x4
+	union {
+		DWORD TitleID; // 0xC sz:0x4
+		struct {
+			WORD PublisherID; // 0xC sz:0x2
+			WORD GameID; // 0xE sz:0x2
+		};
+	};
+	BYTE Platform; // 0x10 sz:0x1
+	BYTE ExecutableType; // 0x11 sz:0x1
+	BYTE DiscNum; // 0x12 sz:0x1
+	BYTE DiscsInSet; // 0x13 sz:0x1
+	DWORD SaveGameID; // 0x14 sz:0x4
+} XEX_EXECUTION_ID, *PXEX_EXECUTION_ID; // size 24
+C_ASSERT(sizeof(XEX_EXECUTION_ID) == 0x18);
+
+typedef struct _STRING {
+    USHORT Length;
+    USHORT MaximumLength;
+    PCHAR Buffer;
+} STRING, *PSTRING;
+
+typedef struct _UNICODE_STRING {
+	USHORT Length;
+	USHORT MaximumLength;
+	PWSTR  Buffer;
+} UNICODE_STRING, *PUNICODE_STRING;
+
+typedef struct _LDR_DATA_TABLE_ENTRY { 
+	LIST_ENTRY InLoadOrderLinks;  // 0x0 sz:0x8
+	LIST_ENTRY InClosureOrderLinks;  // 0x8 sz:0x8
+	LIST_ENTRY InInitializationOrderLinks; // 0x10 sz:0x8
+	PVOID NtHeadersBase; // 0x18 sz:0x4
+	PVOID ImageBase; // 0x1C sz:0x4
+	DWORD SizeOfNtImage; // 0x20 sz:0x4
+	UNICODE_STRING FullDllName; // 0x24 sz:0x8
+	UNICODE_STRING BaseDllName; // 0x2C sz:0x8
+	DWORD Flags; // 0x34 sz:0x4
+	DWORD SizeOfFullImage; // 0x38 sz:0x4
+	PVOID EntryPoint; // 0x3C sz:0x4
+	WORD LoadCount; // 0x40 sz:0x2
+	WORD ModuleIndex; // 0x42 sz:0x2
+	PVOID DllBaseOriginal; // 0x44 sz:0x4
+	DWORD CheckSum; // 0x48 sz:0x4
+	DWORD ModuleLoadFlags; // 0x4C sz:0x4
+	DWORD TimeDateStamp; // 0x50 sz:0x4
+	PVOID LoadedImports; // 0x54 sz:0x4
+	PVOID XexHeaderBase; // 0x58 sz:0x4
+	union{
+		STRING LoadFileName; // 0x5C sz:0x8
+		struct {
+			PVOID ClosureRoot; // 0x5C sz:0x4 LDR_DATA_TABLE_ENTRY
+			PVOID TraversalParent; // 0x60 sz:0x4 LDR_DATA_TABLE_ENTRY
+		} asEntry;
+	} inf;
+} LDR_DATA_TABLE_ENTRY, *PLDR_DATA_TABLE_ENTRY; // size 100
+
+#define NTSTATUS long
+
+extern "C"
+{
+	NTSYSAPI
+	NTSTATUS
+	NTAPI
+	XamGetExecutionId(
+		IN OUT	PXEX_EXECUTION_ID* xid
+	);
+
+	NTSYSAPI
+	DWORD
+	NTAPI
+	XamGetCurrentTitleId(
+		VOID
+	);
+
+	extern PLDR_DATA_TABLE_ENTRY* XexExecutableModuleHandle;
+}
+
 void InitializeGameLib();
 
 HANDLE CreateSystemThread(void* Function, void* Parameter, bool Close);
--- NX1Loader/NX1Loader.cpp
+++ NX1Loader/NX1Loader.cpp
@@ -1,17 +1,78 @@
 #include <stdio.h>
 #include <xtl.h>
+#include <string>
 #include <vector>
+#include <cstdint>
+#include <cwctype>
+#include <algorithm>
 
 #include "GameLib.h"
+
+struct GameOffsets
+{
+	std::wstring module_name;
+	std::uint32_t date_timestamp;
+	std::uint32_t cwd;
+	std::uint32_t scr_begin_load_scripts;
+	std::uint32_t scr_read_file_fastfile;
+	std::uint32_t fs_fopen_file_read_for_thread;
+	std::uint32_t fs_file_read;
+	std::uint32_t fs_file_close;
+	std::uint32_t scr_add_source_buffer_internal;
+};
+
+GameOffsets game_offsets[8] = 
+{
+	{ L"0-Convoy Test 1_5.mp.xex", 0x4F05FDAC, 0x825967D8, 0x822FE2D8, 0x822FEF38, 0x8235BFC0, 0x822CAF90, 0x822CB010, 0 },
+	{ L"0-Nightly MP maps.mp.xex", 0x4F0A8382, 0x82940590, 0x82480250, 0x824821B8, 0x825096F8, 0x82428438, 0x82428958, 0x82481F78 },
+	{ L"1-nx1sp.xex", 0x4EEEC57E, 0x8282EFD8, 0x82465720, 0x82468AD8, 0x824C2B28, 0x82423F48, 0x82424468, 0x82468818 },
+	{ L"2-nx1mp.xex", 0x4EEEC590, 0x829A0510, 0x82497148, 0x82499FA0, 0x8252E9D0, 0x82439748, 0x82439C68, 0x82499CE0 },
+	{ L"3-nx1sp_fast_server.xex", 0x4EEEC5C5, 0x826FEFD8, 0x823D15A0, 0x823D36C0, 0x8241AE30, 0x8239A7E0, 0x8239AC28, 0x823D33E8 },
+	// { L"4-nx1sp_demo.xex", 0, 0, 0, 0, 0, 0, 0, 0 }, // This executable seems to be completely broken
+	{ L"5-nx1mp_demo.xex", 0x4F05FDAC, 0x825967D8, 0x822FE2D8, 0x822FEF38, 0x8235BFC0, 0x822CAF90, 0x822CB010, 0 },
+	{ L"6-nx1mp_fast_server.xex", 0x4F0A8382, 0x82940590, 0x82480250, 0x824821B8, 0x825096F8, 0x82428438, 0x82428958, 0x82481F78 },
+};
+
+GameOffsets* LoadForSupportedGame(std::uint32_t title_id, PLDR_DATA_TABLE_ENTRY data_table_entry)
+{
+	auto date_timestamp = data_table_entry->TimeDateStamp;
+	
+	// Make sure this is being loaded on the correct title
+	if (title_id != 0x4156089E)
+	{
+		printf("Invalid TitleID - Title ID: 0x%X - BaseDllName: %ws - Timestamp: %X\n", title_id, data_table_entry->BaseDllName.Buffer, data_table_entry->TimeDateStamp);
+		return nullptr;
+	}
+	
+	// Attempt to find correct game executable
+	for (auto i = 0; i < ARRAYSIZE(game_offsets); i++)
+	{
+		// Compare against module name and timestamp
+		if (!wcsicmp(data_table_entry->BaseDllName.Buffer, game_offsets[i].module_name.data()) && data_table_entry->TimeDateStamp == game_offsets[i].date_timestamp)
+		{
+			return &game_offsets[i];
+		}
+
+		// Some people may have patched executables but the timestamp should not change
+		if (data_table_entry->TimeDateStamp == game_offsets[i].date_timestamp)
+		{
+			return &game_offsets[i];
+		}
+	}
+
+	// This is a new executable potentially
+	printf("Failed to find compatible executable - Title ID: 0x%X - BaseDllName: %ws - Timestamp: %X\n", title_id, data_table_entry->BaseDllName.Buffer, data_table_entry->TimeDateStamp);
+	return nullptr;
+}
 
 Detour* Scr_BeginLoadScripts_t = nullptr;
 Detour* Scr_ReadFile_FastFile_t = nullptr;
 
-int(__cdecl * FS_FOpenFileReadForThread)(const char* path, int* h, bool unk) = (decltype(FS_FOpenFileReadForThread))0x824C2B28;
-int(__cdecl * FS_FileRead)(void* buf, int len, int h) = (decltype(FS_FileRead))0x82423F48;
-void(__cdecl * FS_FileClose)(int h) = (decltype(FS_FileClose))0x82424468;
+int(__cdecl * FS_FOpenFileReadForThread)(const char* path, int* h, bool isAsync) = nullptr;
+int(__cdecl * FS_FileRead)(void* buf, int len, int h) = nullptr;
+void(__cdecl * FS_FileClose)(int h) = nullptr;
 
-void(__cdecl * Scr_AddSourceBufferInternal)(const char *extFilename, const char *codePos, char *sourceBuf, int len, bool doEolFixup, bool archive, bool newBuffer) = (decltype(Scr_AddSourceBufferInternal))0x82468818;
+void(__cdecl * Scr_AddSourceBufferInternal)(const char *extFilename, const char *codePos, char *sourceBuf, int len, bool doEolFixup, bool archive, bool newBuffer) = nullptr;
 
 std::vector<char*> cached_scripts;
 
@@ -46,7 +107,10 @@ char * Scr_ReadFile_FastFile(const char *filename, const char *extFilename, cons
 	FS_FileRead(script_buffer, file_size, file_handle);
 	FS_FileClose(file_handle);
 
-	Scr_AddSourceBufferInternal(extFilename, codePos, script_buffer, file_size, true, archive, true);
+	if (Scr_AddSourceBufferInternal != nullptr)
+	{
+		Scr_AddSourceBufferInternal(extFilename, codePos, script_buffer, file_size, true, archive, true);
+	}
 
 	printf("Loaded Script File: %s\n", filename);
 
@@ -60,11 +124,24 @@ BOOL __stdcall DllMain(HANDLE hHandle, DWORD dwReason, LPVOID lpReserved)
 	{
 		printf("NX1Loader Loaded!\n");
 		
-		// Replace D: with game: to allow reading using game functions
-		strcpy(reinterpret_cast<char*>(0x8282EFD8), "game:");
+		if (auto* game = LoadForSupportedGame(XamGetCurrentTitleId(), *XexExecutableModuleHandle))
+		{
+			// Initialize Functions
+			FS_FOpenFileReadForThread = (decltype(FS_FOpenFileReadForThread))game->fs_fopen_file_read_for_thread;
+			FS_FileRead = (decltype(FS_FileRead))game->fs_file_read;
+			FS_FileClose = (decltype(FS_FileClose))game->fs_file_close;
+			Scr_AddSourceBufferInternal = (decltype(Scr_AddSourceBufferInternal))game->scr_add_source_buffer_internal;
 
-		Scr_BeginLoadScripts_t = new Detour(0x82465720, reinterpret_cast<DWORD>(Scr_BeginLoadScripts));
-		Scr_ReadFile_FastFile_t = new Detour(0x82468AD8, reinterpret_cast<DWORD>(Scr_ReadFile_FastFile));
+			// Replace D: with game: to allow reading using game functions
+			strcpy(reinterpret_cast<char*>(game->cwd), "game:");
+
+			Scr_BeginLoadScripts_t = new Detour(game->scr_begin_load_scripts, reinterpret_cast<DWORD>(Scr_BeginLoadScripts));
+			Scr_ReadFile_FastFile_t = new Detour(game->scr_read_file_fastfile, reinterpret_cast<DWORD>(Scr_ReadFile_FastFile));
+
+			return TRUE;
+		}
+
+		return FALSE;
 	}
 	else if (dwReason == DLL_PROCESS_DETACH)
 	{
